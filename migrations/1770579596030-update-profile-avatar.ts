eb39828499c5c19167dcaaa0af2404b8bd647830
l4ySS <superlol441@gmail.com>
2026-03-02 12:03:15 +0300

    [homework-2] fix: обернул запросы в миграциях в транзакции

--- migrations/1770579596030-update-profile-avatar.ts
+++ migrations/1770579596030-update-profile-avatar.ts
@@ -4,59 +4,76 @@ export class InitProfileRefreshTokenAvatar1700000000000 implements MigrationInte
   name = 'InitProfileRefreshTokenAvatar1700000000000';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-      CREATE TABLE "profile" (
-        "id" SERIAL NOT NULL,
-        "login" VARCHAR NOT NULL,
-        "email" VARCHAR NOT NULL,
-        "password" VARCHAR NOT NULL,
-        "age" INTEGER NOT NULL,
-        "description" VARCHAR(1000) NOT NULL,
-        "deletedAt" TIMESTAMP,
-        CONSTRAINT "PK_profile_id" PRIMARY KEY ("id"),
-        CONSTRAINT "UQ_profile_login" UNIQUE ("login"),
-        CONSTRAINT "UQ_profile_email" UNIQUE ("email")
-      )
-    `);
+    await queryRunner.startTransaction();
 
-    await queryRunner.query(`
-      CREATE TABLE "refresh_token" (
-        "id" SERIAL NOT NULL,
-        "jti" VARCHAR NOT NULL,
-        "tokenHash" VARCHAR NOT NULL,
-        "expiresAt" TIMESTAMP NOT NULL,
-        "revoked" BOOLEAN NOT NULL DEFAULT false,
-        "profile_id" INTEGER,
-        CONSTRAINT "PK_refresh_token_id" PRIMARY KEY ("id"),
-        CONSTRAINT "UQ_refresh_token_jti" UNIQUE ("jti"),
-        CONSTRAINT "FK_refresh_token_profile"
-          FOREIGN KEY ("profile_id")
-          REFERENCES "profile"("id")
-          ON DELETE NO ACTION
-          ON UPDATE NO ACTION
-      )
-    `);
+    try {
+      await queryRunner.query(`
+        CREATE TABLE "profile" (
+          "id" SERIAL NOT NULL,
+          "login" VARCHAR NOT NULL,
+          "email" VARCHAR NOT NULL,
+          "password" VARCHAR NOT NULL,
+          "age" INTEGER NOT NULL,
+          "description" VARCHAR(1000) NOT NULL,
+          "deletedAt" TIMESTAMP,
+          CONSTRAINT "PK_profile_id" PRIMARY KEY ("id"),
+          CONSTRAINT "UQ_profile_login" UNIQUE ("login"),
+          CONSTRAINT "UQ_profile_email" UNIQUE ("email")
+        )
+      `);
 
-    await queryRunner.query(`
-      CREATE TABLE "avatar" (
-        "id" SERIAL NOT NULL,
-        "fileName" VARCHAR NOT NULL,
-        "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
-        "deletedAt" TIMESTAMP,
-        "profile_id" INTEGER,
-        CONSTRAINT "PK_avatar_id" PRIMARY KEY ("id"),
-        CONSTRAINT "FK_avatar_profile"
-          FOREIGN KEY ("profile_id")
-          REFERENCES "profile"("id")
-          ON DELETE CASCADE
-          ON UPDATE NO ACTION
-      )
-    `);
+      await queryRunner.query(`
+        CREATE TABLE "refresh_token" (
+          "id" SERIAL NOT NULL,
+          "jti" VARCHAR NOT NULL,
+          "tokenHash" VARCHAR NOT NULL,
+          "expiresAt" TIMESTAMP NOT NULL,
+          "revoked" BOOLEAN NOT NULL DEFAULT false,
+          "profile_id" INTEGER,
+          CONSTRAINT "PK_refresh_token_id" PRIMARY KEY ("id"),
+          CONSTRAINT "UQ_refresh_token_jti" UNIQUE ("jti"),
+          CONSTRAINT "FK_refresh_token_profile"
+            FOREIGN KEY ("profile_id")
+            REFERENCES "profile"("id")
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+        )
+      `);
+
+      await queryRunner.query(`
+        CREATE TABLE "avatar" (
+          "id" SERIAL NOT NULL,
+          "fileName" VARCHAR NOT NULL,
+          "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+          "deletedAt" TIMESTAMP,
+          "profile_id" INTEGER,
+          CONSTRAINT "PK_avatar_id" PRIMARY KEY ("id"),
+          CONSTRAINT "FK_avatar_profile"
+            FOREIGN KEY ("profile_id")
+            REFERENCES "profile"("id")
+            ON DELETE CASCADE
+            ON UPDATE NO ACTION
+        )
+      `);
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "avatar"`);
-    await queryRunner.query(`DROP TABLE "refresh_token"`);
-    await queryRunner.query(`DROP TABLE "profile"`);
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.query(`DROP TABLE "avatar"`);
+      await queryRunner.query(`DROP TABLE "refresh_token"`);
+      await queryRunner.query(`DROP TABLE "profile"`);
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    }
   }
 }
--- migrations/1771798897596-ProfileAvatarIndex.ts
+++ migrations/1771798897596-ProfileAvatarIndex.ts
@@ -2,27 +2,41 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class ProfileAvatarIndex1771798897596 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-        CREATE INDEX idx_profile_active_age
-        ON profile (age, id)
-        WHERE "deletedAt" IS NULL
-            AND "description" IS NOT NULL
-        `);
+    await queryRunner.startTransaction();
+    try {
+      await queryRunner.query(`
+          CREATE INDEX idx_profile_active_age
+          ON profile (age, id)
+          WHERE "deletedAt" IS NULL
+              AND "description" IS NOT NULL
+          `);
 
-    await queryRunner.query(`
-        CREATE INDEX idx_avatar_profile_created_desc
-        ON avatar (profile_id, "createdAt" DESC)
-        WHERE "deletedAt" IS NULL
-        `);
+      await queryRunner.query(`
+          CREATE INDEX idx_avatar_profile_created_desc
+          ON avatar (profile_id, "createdAt" DESC)
+          WHERE "deletedAt" IS NULL
+          `);
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-        DROP INDEX idx_avatar_profile_created_desc
-        `);
+    await queryRunner.startTransaction();
+    try {
+      await queryRunner.query(`
+          DROP INDEX idx_avatar_profile_created_desc
+          `);
 
-    await queryRunner.query(`
-        DROP INDEX idx_profile_active_age
-        `);
+      await queryRunner.query(`
+          DROP INDEX idx_profile_active_age
+          `);
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    }
   }
 }
